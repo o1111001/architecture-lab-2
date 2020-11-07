3be43f579aca8e96ffdf0e6105aa803dc819e256
Yaroslav Chugai <yaroslavchugai@gmail.com>
2020-11-07 20:54:24 +0200

    feat: Implementation

--- implementation.go
+++ implementation.go
@@ -1,9 +1,64 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strings"
+	"strconv"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+type fnType func (float64, float64) float64
+
+func plus(a float64, b float64) float64 {
+	return b + a
+}
+
+func minus(a float64, b float64) float64 {
+	return b - a
+}
+
+func mult(a float64, b float64) float64 {
+	return b * a
+}
+
+func divide(a float64, b float64) float64 {
+	return b / a
+}
+
+func pow(a float64, b float64) float64 {
+	return math.Pow(b, a)
+}
+
+var functionsMap = map[string] fnType {
+	"+": plus,
+	"-": minus,
+	"/": divide,
+	"*": mult,
+	"^": pow,
+}
+
+func stringToFloat(str string) (float64, error) {
+	return strconv.ParseFloat(str, 64)
+}
+
+func EvaluatePostfix(input string) (string, error) {
+	if strings.TrimSpace(input) == "" {
+		return "", fmt.Errorf("Expression is empty string")
+	}
+	var stack []float64
+	s := strings.Split(input, " ")
+
+	for _, element := range s {
+		if fn, ok := functionsMap[element]; ok {
+			a, b := stack[len(stack)-1], stack[len(stack)-2]
+			stack = append(stack[:len(stack)-2], fn(a, b))
+		} else {
+			floatNumber, err := stringToFloat(element)
+			if err != nil {
+				return "", fmt.Errorf("Cannot parse expression")
+			}
+			stack = append(stack, floatNumber)
+		}
+	}
+	return stack[0], nil
 }
